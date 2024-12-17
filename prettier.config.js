68accf1ec307bb05866e2cf42afbe7e81e56a2e4
Mike MacCana <mike.maccana@gmail.com>
2024-12-17 14:10:35 -0500

    Use prettier file format with comments

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+// Normally I hate modifying prettier defaults as they're usually nitpicky, but web3.js tends to use longer names, 
+// so we need to increase the print width to avoid single function calls spilling over into multiple lines
+
+module.exports = {
+    "printWidth": 120
+}
