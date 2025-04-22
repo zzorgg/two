a29eeafd57716bef96b4f781416de022d041a574
Mike MacCana <mike.maccana@gmail.com>
2025-04-22 14:19:13 -0400

    Show OS version cleanly

--- show-versions.sh
+++ show-versions.sh
@@ -1,5 +1,13 @@
 
-echo -e "OS: \n  $(lsb_release -a 2> /dev/null || sw_vers -productVersion)"
+get_os_version() {
+    if command -v lsb_release &> /dev/null; then
+        echo "$(lsb_release -d | cut -f2)"
+    else
+        echo "MacOS $(sw_vers -productVersion)"
+    fi
+}
+
+echo -e "OS: \n  $(get_os_version)"
 echo -e "Solana CLI: \n  $(solana -V)"
 echo -e "Anchor: \n  $(anchor --version)"
 echo -e "Node: \n  $(node --version)"
