b7845d1f3cc61d000db77043b2f2c3a70a8bda8c
Mike MacCana <mike.maccana@gmail.com>
2025-04-26 02:28:05 -0400

    Revert "Don't download things when checking build-sbf version"
    This reverts commit 6e6ad6d35e3a47e8c888dc91c05976ac0c7caca4.

--- show-versions.sh
+++ show-versions.sh
@@ -1,3 +1,4 @@
+
 get_os_version() {
     if command -v lsb_release &> /dev/null; then
         echo "$(lsb_release -d | cut -f2)"
@@ -12,7 +13,7 @@ echo -e "Anchor: \n  $(anchor --version)"
 echo -e "Node: \n  $(node --version)"
 echo -e "Rust: \n  $(rustc -V)"
 # Cut the first line of the output
-echo -e "build-sbf version: \n  $(solana-cargo-build-sbf --version | head -n 1)"
+echo -e "build-sbf version: \n  $(cargo build-sbf --version | head -n 1)"
 echo -e "proc-macro2: \n  $(cargo tree -p proc-macro2 | head -n 1)"
 
 echo -e "\n\nPath: \n$(echo $PATH | tr ':' '\n' | sort)"
