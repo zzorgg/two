8b6d53cbf88158a9c2573b7b39620c594e35d239
Mike MacCana <mike.maccana@gmail.com>
2025-06-16 14:50:34 -0400

    Make confetti stop after 5 seconds

--- confetti.ts
+++ confetti.ts
@@ -1,7 +1,15 @@
 import cliConfetti from "cli-confetti";
 import CliUpdate from "cli-update";
 
+const SECONDS = 1000;
+
 cliConfetti({}, function (error, confetti) {
   if (error) throw error;
   CliUpdate.render(confetti);
+
+  // Stop confetti after 5 seconds
+  setTimeout(() => {
+    CliUpdate.render("🎉 Congratulations on passing all the tests! 🎉");
+    process.exit(0);
+  }, 5 * SECONDS);
 });
