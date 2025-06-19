bda3ed398eb3e7cc934370bc57b050c20374ac95
Mike MacCana <mike.maccana@gmail.com>
2025-06-18 20:57:02 -0400

    Use better confetti

--- confetti.ts
+++ confetti.ts
@@ -3,13 +3,21 @@ import CliUpdate from "cli-update";
 
 const SECONDS = 1000;
 
-cliConfetti({}, function (error, confetti) {
-  if (error) throw error;
-  CliUpdate.render(confetti);
+cliConfetti(
+  {
+    // The config
+    chars: ["✨", "🍾", "🥂", "😃", "💯", "💙", "CONGRATULATIONS!"],
+  },
+  function (error, confetti) {
+    if (error) throw error;
+    // @ts-expect-error CluUpdate uses some kind of pre-ES6 class syntax that freaks out type inference
+    CliUpdate.render(confetti);
 
-  // Stop confetti after 5 seconds
-  setTimeout(() => {
-    CliUpdate.render("🎉 Congratulations on passing all the tests! 🎉");
-    process.exit(0);
-  }, 5 * SECONDS);
-});
+    // Stop confetti after 5 seconds
+    setTimeout(() => {
+      // @ts-expect-error See above
+      CliUpdate.render("🎉 Congratulations from QuickNode on building your first Solana program! 🎉");
+      process.exit(0);
+    }, 5 * SECONDS);
+  },
+);
